d7d3927c75ffdfa34179a9534e540172c91c6bbd
Derek Campbell <dec@Systems-iMac.local>
2017-10-26 15:59:53 -0700

    added message and updated alphabet

--- StrangerThings.ino
+++ StrangerThings.ino
@@ -26,29 +26,40 @@
 #include <platforms.h>
 #include <power_mgt.h>
 
-#define NUM_LEDS 60
+#define NUM_LEDS 50
 CRGB leds[NUM_LEDS];
 
 // variable to hold LED integers that correspond to leds[int] and alphabet
-int letters[26];
+int letters[30];
 
 // arduino setup function
 void setup() {
   Serial.begin(9600);
-  FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2811, 6>(leds, NUM_LEDS);
   setupLetters(); // setup our letters first
   clearLeds(); // clear the leds just in case
 }
 
 // arduino main loop function
 void loop() {
-  message("I WILL KILL YOU", 1000);
-  delay(1000);
-  message("I WANT BRAINS DAMMIT", 1000);
-  delay(1000);
-
+  fadeInLeds();
+  delay(2000);
+  fadeOutLeds();
+  delay(2000);
+  message("ESC@P# V!$", 500);
+  fadeOutLeds();
+  delay(2000);
   // if we are going to have user inputted messages, the code should go here to read from the serial
   // and run the message function
+}
+
+// not sure if we want to use this function
+void turnOnAllLights(){
+  FastLED.clear();
+  for(int led = 0; led < NUM_LEDS; led++) {
+    leds[led] = CRGB::White;
+  }
+  FastLED.show();
 }
 
 void setupLetters(){
@@ -62,13 +73,13 @@ void setupLetters(){
   letters[1] = baseLED + 1;
 
   // letter C
-  letters[2] = baseLED + 2;
+  letters[2] = baseLED + 12;
 
   // letter D
   letters[3] = baseLED + 3;
 
   // letter E
-  letters[4] = baseLED + 4;
+  letters[4] = baseLED + 14;
 
   // letter F
   letters[5] = baseLED + 5;
@@ -89,7 +100,7 @@ void setupLetters(){
   letters[10] = baseLED + 14;
 
   // letter L
-  letters[11] = baseLED + 13;
+  letters[11] = baseLED + 18;
 
   // letter M
   letters[12] = baseLED + 12;
@@ -98,10 +109,10 @@ void setupLetters(){
   letters[13] = baseLED + 11;
 
   // letter O
-  letters[14] = baseLED + 10;
+  letters[14] = baseLED + 2;
 
   // letter P
-  letters[15] = baseLED + 9;
+  letters[15] = baseLED + 10;
 
   // letter Q
   letters[16] = baseLED + 8;
@@ -110,7 +121,7 @@ void setupLetters(){
   letters[17] = baseLED + 17;
 
   // letter S
-  letters[18] = baseLED + 18;
+  letters[18] = baseLED + 13;
 
   // letter T
   letters[19] = baseLED + 19;
@@ -119,7 +130,7 @@ void setupLetters(){
   letters[20] = baseLED + 20;
 
   // letter V
-  letters[21] = baseLED + 21;
+  letters[21] = baseLED + 20;
 
   // letter W
   letters[22] = baseLED + 22;
@@ -133,18 +144,33 @@ void setupLetters(){
   // letter Z
   letters[25] = baseLED + 25;
 
+  // letter A2 [@]
+  letters[26] = baseLED + 11;
+
+  // letters E2 [#]
+  letters[27] = baseLED + 9;
+
+  // letters I2 [!]
+  letters[28] = baseLED + 21;
+
+  // letters P2 [$]
+  letters[29] = baseLED + 22;
+
+
 }
 
 // function to clear ALL of the LEDS
 void clearLeds() {
   for(int i = 0; i < NUM_LEDS; i++){
     leds[i] = CRGB::Black;
-    FastLED.show();
   }
+  FastLED.show();
 }
 
-// hopefully this method is non-blocking 
+// hopefully this method is non-blocking
 void fadeOutLeds(){
+
+  Serial.println("fading out leds...");
 
   int animationTime = 500;
 
@@ -160,10 +186,33 @@ void fadeOutLeds(){
     for(int i = 0; i < NUM_LEDS; i++){
       leds[i].fadeToBlackBy(ledAmount);
     }
+    FastLED.show();
     delta = millis() - start;
   }
+}
 
+// hopefully this method is non-blocking 
+void fadeInLeds(){
 
+  Serial.println("fading in leds...");
+
+  int animationTime = 500;
+
+  int startLED = 0;
+  int endLED = 256;
+
+  unsigned long start = millis();
+  unsigned long delta = millis() - start;
+
+  while(delta < animationTime) {
+    float pos = float(delta) / float(animationTime);
+    int ledAmount = lerp(pos, 0.0, 1.0, startLED, endLED);
+    for(int i = 0; i < NUM_LEDS; i++){
+      leds[i].fadeLightBy(ledAmount);
+    }
+    FastLED.show();
+    delta = millis() - start;
+  }
 }
 
 // linear interpolation function
@@ -216,105 +265,121 @@ void message(String words, int delayLength) {
     if (letter == "A") {
        letterInt = 0;
     }
-    
+
     if (letter == "B") {
        letterInt = 1;
     }
-    
+
     if (letter == "C") {
        letterInt = 2;
     }
-    
+
     if (letter == "D") {
        letterInt = 3;
     }
-    
+
     if (letter == "E") {
        letterInt = 4;
     }
-    
+
     if (letter == "F") {
        letterInt = 5;
     }
-    
+
     if (letter == "G") {
        letterInt = 6;
     }
-    
+
     if (letter == "H") {
        letterInt = 7;
     }
-    
+
     if (letter == "I") {
        letterInt = 8;
     }
-    
+
     if (letter == "J") {
        letterInt = 9;
     }
-    
+
     if (letter == "K") {
        letterInt = 10;
     }
-    
+
     if (letter == "L") {
        letterInt = 11;
     }
-    
+
     if (letter == "M") {
        letterInt = 12;
     }
-    
+
     if (letter == "N") {
        letterInt = 13;
     }
-    
+
     if (letter == "O") {
        letterInt = 14;
     }
-    
+
     if (letter == "P") {
        letterInt = 15;
     }
-    
+
     if (letter == "Q") {
        letterInt = 16;
     }
-    
+
     if (letter == "R") {
        letterInt = 17;
     }
-    
+
     if (letter == "S") {
        letterInt = 18;
     }
-    
+
     if (letter == "T") {
        letterInt = 19;
     }
-    
+
     if (letter == "U") {
        letterInt = 20;
     }
-    
+
     if (letter == "V") {
        letterInt = 21;
     }
-    
+
     if (letter == "W") {
        letterInt = 22;
     }
-    
+
     if (letter == "X") {
        letterInt = 23;
     }
-    
+
     if (letter == "Y") {
        letterInt = 24;
     }
-    
+
     if (letter == "Z") {
        letterInt = 25;
+    }
+
+    if (letter == "@") {
+      letterInt = 26;
+    }
+
+    if (letter == "#") {
+      letterInt = 27;
+    }
+
+    if (letter == "!") {
+      letterInt = 28;
+    }
+
+    if (letter == "$") {
+      letterInt = 29;
     }
 
 
@@ -331,7 +396,7 @@ void message(String words, int delayLength) {
       Serial.print("SHOWING LETTER -");
       Serial.println(letter);
       Serial.println("");
-      
+
     }
 
     if(letterInt < 0){
